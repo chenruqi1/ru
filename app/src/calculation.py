4190ddc0b78778948cda959db7731a4045c876d6
chenruqi1 <ruqichen@hotmail.com>
2022-07-23 14:37:23 -0500

    add my files1

---
+++ app/src/calculation.py
@@ -0,0 +1,12 @@
+class Calculation():
+    def add(self,num1,num2):
+        return num1+num2
+    def substract(self,num1,num2):
+        return num1-num2
+    def multiply(self,num1,num2):
+        return num1*num2
+    def divide(self,num1,nums):
+        if num2==0:
+            return 0
+        return num1/num2
+
